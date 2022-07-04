d46f20307f5af0ccc2b92d17f719424c5262009a
sofia_frugone <sofiabellaf2000@gmail.com>
2022-07-04 16:54:41 +1000

    added categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = ["Face & Complexion", "Cheeks & Contour", "Eye Makeup", "Eyebrow Makeup", "Lips", "Brushes & Tools"]
+
+if Category.count == 0
+    categories.each do |category|
+        Category.create(name: category)
+        puts "created #{category} category"
+    end
+end
